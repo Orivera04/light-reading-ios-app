4cc74f42d89b94ad2ee9c1ad6c50396ebbff883e
orivera04 <riveramoreira04@gmail.com>
2024-02-06 10:50:03 -0600

    Feat: Init project.

---
+++ light-reading-meter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  light-reading-meter
+//
+//  Created by Oscar Rivera Moreira on 6/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
